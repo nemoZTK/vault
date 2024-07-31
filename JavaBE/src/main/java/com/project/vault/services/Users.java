a9dc4d8c4a98de9433b07608c37ce412550f349b
emir cogorno <nemo.ztk@gmail.com>
2024-07-31 18:14:52 +0200

    java be
    created a table vaultUsers and a little controller to test it
    ( it work)

---
+++ JavaBE/src/main/java/com/project/vault/services/Users.java
@@ -0,0 +1,54 @@
+package com.project.vault.services;
+
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+public class Users {
+
+}
+
+interface UserRepo extends ListCrudRepository<VaultUsers, Long> {
+
+}
+
+@Controller
+@ResponseBody
+class UsersController {
+
+	private final UserRepo userRepo;
+
+	UsersController(UserRepo userRepo) {
+		this.userRepo = userRepo;
+	}
+
+	@GetMapping("/all")
+	List<VaultUsers> getAllUsers() {
+		List<VaultUsers> allUsers = this.userRepo.findAll();
+		System.out.println("finded {" + allUsers.toArray().toString() + "}");
+		return allUsers;
+	}
+
+	@PostMapping("/create")
+	void create(@RequestBody VaultUsers user) {
+		userRepo.save(user);
+		System.out.println("saved{" + user + "}");
+	}
+
+}
+
+@Table(name = "vault_users")
+record VaultUsers(@Id Long id, String username, String email, String password, String path) {
+
+}
+
+//@Table(name = "topic_space")
+//record TopicSpace(@Id Long id, String name, String description, String path) {
+//}
---
+++ JavaBE/src/main/resources/tables.sql
@@ -0,0 +1,7 @@
+CREATE TABLE vault_users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    path VARCHAR(255) NULL
+);
