9ddf8f44d1f3a61c5b28a1a85106f4a91b26b0e5
emir cogorno <nemo.ztk@gmail.com>
2024-09-07 13:30:23 +0200

    fix commit
    some conflict errors in previus commit solved

--- JavaBE/src/main/java/com/project/vault/services/storage/StorageServiceInterface.java
+++ JavaBE/src/main/java/com/project/vault/services/storage/StorageServiceInterface.java
@@ -4,7 +4,6 @@ import org.json.JSONObject;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.project.vault.entities.VaultFile;
-import com.project.vault.entities.VaultFolder;
 import com.project.vault.entities.VaultSpace;
 
 public interface StorageServiceInterface {
@@ -32,19 +31,14 @@ public interface StorageServiceInterface {
 	 */
 	JSONObject holdNewFolderRequest(Long userId, Long spaceId, Long parentId, String name);
 
-	Boolean isHimTheFolderOwner(Long folderId, Long userId);
-
 	// ---------spaces
 	VaultSpace saveNewSpace(String name, Long userId);
-
-	JSONObject holdNewSpaceRequest(String name, Long userId);
-
-	Boolean isHimTheSpaceOwner(Long spaceId, Long userId);
 
 	VaultSpace getSpaceById(Long id);
 
 	// ----files
 	VaultFile saveNewFile(MultipartFile file, Long userId, Long spaceId, Long parentId);
+
 	/**
 	 * save a new space with the given in local and in db, then return a feedback
 	 * that say if is done and what is going wrong
--- VueFE/src/components/InsertNameForm.vue
+++ VueFE/src/components/InsertNameForm.vue
@@ -12,60 +12,21 @@
   export default {
     components: {
       BaseForm
-  <BaseForm title="Inserisci Nome" submitButtonText="Conferma" @submit="submit" @close="closeForm">
-    <template v-slot:form-fields>
-      <input type="text" v-model="name" placeholder="Nome" required />
-    </template>
-  </BaseForm>
-</template>
-
-<script>
-import BaseForm from './BaseForm.vue';
-
-export default {
-  components: {
-    BaseForm
-  },
-  data() {
-    return {
-      name: ''
-    };
-  },
-  methods: {
-    submit() {
-      console.log("clicked on confirm for "+this.name);
-      
-      this.$emit('submit', this.name);
     },
-    closeForm() {
-      this.$emit('close');
+    data() {
+      return {
+        name: ''
+      };
+    },
+    methods: {
+      submit() {
+        console.log("clicked on confirm for "+this.name);
+        
+        this.$emit('submit', this.name);
+      },
+      closeForm() {
+        this.$emit('close');
+      }
     }
-  }
-};
-</script>
-
-
-<style scoped>
-/* Stili specifici per InsertNameForm */
-
-.form-container {
-  position: fixed;
-  top: 20%; /* Modifica questa posizione se necessario */
-  left: 50%;
-  transform: translateX(-50%);
-  z-index: 1000; /* Assicurati che sia sopra gli altri elementi */
-  /* Aggiungi ulteriori stili se necessario */
-}
-
-.form-container h2 {
-  margin: 0; /* Aggiusta il margine se necessario */
-}
-
-.form-container input {
-  width: 100%; /* Assicurati che l'input si adatti al contenitore */
-}
-
-.form-container button {
-  width: 100%; /* Assicurati che i pulsanti si adattino al contenitore */
-}
-</style>
+  };
+  </script>
